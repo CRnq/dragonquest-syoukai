705e78c9f2564bc597f2c2650634f39cecedcc24
CRnq <0611yurayura@gmail.com>
2024-02-17 19:52:17 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function($) {
+    $('.bg-switcher').bgSwitcher({
+     images:['images/DQ1title.jpg','images/DQ2title.jpg','images/DQ3title.jpg','images/DQ4title.jpg','images/DQ5title.jpg','images/DQ6title.jpg','images/DQ7title.jpg','images/DQ8title.jpg','images/DQ9title.jpg','images/DQ10title.jpg','images/DQ11title.jpg','images/DQ12title.jpeg'],
+     interval: 3000,
+     loop: true
+    });
+});
